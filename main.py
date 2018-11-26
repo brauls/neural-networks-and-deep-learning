3b2a848032011e513ccdb5e13ab333000568acf2
Benedikt <19bene89@googlemail.com>
2018-11-26 18:43:42 +0100

    add network2 to main py file

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import src.mnist_loader as mnist_loader
 import src.network as network
+import src.network2 as network2
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-net = network.Network([784, 30, 10])
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+# Network 1
+# net = network.Network([784, 30, 10])
+# net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+# Network 2
+net = network2.Network([784, 30, 10])
+evaluation_cost, evaluation_accuracy, training_cost, training_accuracy = net.SGD(training_data, 30, 10, 0.5, lmbda = 5.0, evaluation_data=validation_data, monitor_evaluation_accuracy=True, monitor_evaluation_cost=True, monitor_training_accuracy=True, monitor_training_cost=True)
